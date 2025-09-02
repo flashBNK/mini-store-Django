c89d561dc3af0ac82779a25d813d38dbdad1cc10
flashBNK <lolmaks23@gmail.com>
2025-09-02 19:08:10 +0300

    Хуй

--- main/models.py
+++ main/models.py
@@ -7,4 +7,12 @@ class Category(models.Model):
 
     class Meta:
         ordering = ('name',)
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
 
+    def __str__(self):
+        return self.name
+
+class Product(models.Model):
+    name = models.CharField(max_length=128)
+    category = models.ForeignKey(Category, related_name='Products', on_delete=models.CASCADE())
