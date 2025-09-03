61ee623d831ec770f2ea65488dea9770fa318f33
flashBNK <lolmaks23@gmail.com>
2025-09-03 15:22:41 +0300

    Сделал модели, зарегали в админке

--- main/models.py
+++ main/models.py
@@ -14,5 +14,18 @@ class Category(models.Model):
         return self.name
 
 class Product(models.Model):
-    name = models.CharField(max_length=128)
-    category = models.ForeignKey(Category, related_name='Products', on_delete=models.CASCADE())
+    category = models.ForeignKey(Category, related_name='Products', on_delete=models.CASCADE)
+    name = models.CharField(max_length=128, db_index=True)
+    slug = models.SlugField(max_length=128, unique=True)
+    image = models.ImageField(upload_to='products/%Y/%m/%d', blank=True)
+    description = models.TextField(blank=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    available = models.BooleanField(default=True)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ('name',)
+
+    def __str__(self):
+        return self.name
