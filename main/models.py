acb598e718225b7984706a72d2d4743180ef9232
flashBNK <lolmaks23@gmail.com>
2025-09-10 13:58:17 +0300

    Законил приложение main

--- main/models.py
+++ main/models.py
@@ -1,9 +1,10 @@
 from django.db import models
+from django.urls import reverse
 
 
 class Category(models.Model):
-    name = models.CharField(max_length=128, db_index=True)
-    slug = models.SlugField(max_length=128, unique=True)
+    name = models.CharField(max_length=100, db_index=True)
+    slug = models.SlugField(max_length=100, unique=True)
 
     class Meta:
         ordering = ('name',)
@@ -13,19 +14,27 @@ class Category(models.Model):
     def __str__(self):
         return self.name
 
+    def get_absolute_url(self):
+        return reverse("main:product_list_by_category", args=[self.slug])
+
+
 class Product(models.Model):
-    category = models.ForeignKey(Category, related_name='Products', on_delete=models.CASCADE)
-    name = models.CharField(max_length=128, db_index=True)
-    slug = models.SlugField(max_length=128, unique=True)
+    category = models.ForeignKey(Category, related_name='products',
+                                 on_delete=models.CASCADE)
+    name = models.CharField(max_length=100, db_index=True)
+    slug = models.SlugField(max_length=100, unique=100)
     image = models.ImageField(upload_to='products/%Y/%m/%d', blank=True)
     description = models.TextField(blank=True)
     price = models.DecimalField(max_digits=10, decimal_places=2)
     available = models.BooleanField(default=True)
     created = models.DateTimeField(auto_now_add=True)
-    updated = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
 
     class Meta:
         ordering = ('name',)
 
     def __str__(self):
         return self.name
+
+    def get_absolute_url(self):
+        return reverse("main:product_detail", args=[self.id, self.slug])
