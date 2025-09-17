9347e3a46ab49e355c781a2557837819280aa5eb
flashBNK <lolmaks23@gmail.com>
2025-09-17 15:24:49 +0300

    сделал корзину и добавил контекст процессора

--- study_store/settings.py
+++ study_store/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'main',
+    'cart'
 ]
 
 MIDDLEWARE = [
@@ -61,9 +62,11 @@ TEMPLATES = [
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
+                'django.template.context_processors.debug',
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'cart.context_processors.cart',
             ],
         },
     },
@@ -127,3 +130,5 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CART_SESSION_ID = 'cart'
--- study_store/urls.py
+++ study_store/urls.py
@@ -1,11 +1,12 @@
-from django.conf import settings
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('main.urls', namespace='main'))
+    path('cart/', include('cart.urls', namespace='cart')),
+    path('', include('main.urls', namespace='main')),
 ]
 
 if settings.DEBUG:
